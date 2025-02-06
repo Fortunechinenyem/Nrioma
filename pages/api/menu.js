f4a853f21df9f28d405561386e87fec28b2464f0
Fortunechinenyem <fortunechinenyem@gmail.com>
2025-02-06 03:14:03 +0100

    updated images

--- pages/api/menu.js
+++ pages/api/menu.js
@@ -1,8 +1,17 @@
 export default function handler(req, res) {
   const menu = [
-    { id: 1, name: "Jollof Rice", price: 1500, image: "/jollof.jpg" },
-    { id: 2, name: "Egusi Soup", price: 2500, image: "/egusi.jpg" },
-    { id: 3, name: "Fried Rice", price: 1800, image: "/fried-rice.jpg" },
+    { id: 1, name: "Jollof Rice", price: 1500, image: "/images/pix (6).png" },
+    { id: 2, name: "Egusi Soup", price: 2500, image: "/images/pix (3).png" },
+    { id: 3, name: "Yam and Egg", price: 1800, image: "/images/pix (5).png" },
+    { id: 4, name: "Ogbono Soup", price: 2500, image: "/images/pix (2).png" },
+    {
+      id: 5,
+      name: "Chips and Chicken",
+      price: 3000,
+      image: "/images/pix (1).png",
+    },
+    { id: 6, name: "Okro Soup", price: 2500, image: "/images/pix (9).png" },
+    { id: 7, name: "Efo riro", price: 2500, image: "/images/pix (10).png" },
   ];
   res.status(200).json(menu);
 }
